48f1caa92db9f66e6504da4c70caa634741eaa61
zoanlogia <56681566+zoanlogia@users.noreply.github.com>
2021-12-09 00:08:08 +0100

    fix template photographer; continue css before inject in template

--- scripts/templates/photographerSolo.js
+++ scripts/templates/photographerSolo.js
@@ -1,26 +1,29 @@
 export class PhotographerSolo {
   constructor(photographer) {
-    this._photographer.info = photographer;
-    this._photographer.media = photographer;
+    this._photographer = photographer;
   }
 
   createPhotographerSolo() {
+    console.log(this._photographer.info);
+    console.log(this._photographer.medias);
     const sectionSolo = document.createElement("div");
     sectionSolo.classList.add("container-card");
-    const photographerSolo = `
-    <article>
-      <a href="photographer.html?id=${this._photographer.info.id}">
-        <img class="photographer_img" src="${this._photographer.portrait}" alt="${this._photographer.name}"/>
-          <h2>${this._photographer.name}</h2>
-      </a>
-      <p class="photographer_city">${this._photographer.city}, ${this._photographer.country}</p>
-      <a href="#">
-        <p class="photographer_tagline">${this._photographer.tagline}</p>
-      </a>
-      <p class="photographer_price">${this._photographer.price}</p>
-    </article>`;
+    // const photographerSolo = `
+    // <div class="container-card">
+    // <article>
+    //   <a href="photographer.html?id=${this._photographer.info.id}">
+    //     <img class="photographer_img" src="assets/photographers/${this._photographer.info.portrait}" alt="${this._photographer.info.name}"/>
+    //       <h2>${this._photographer.info.name}</h2>
+    //   </a>
+    //   <p class="photographer_city">${this._photographer.info.city}, ${this._photographer.info.country}</p>
+    //   <a href="#">
+    //     <p class="photographer_tagline">${this._photographer.info.tagline}</p>
+    //   </a>
+    //   <p class="photographer_price">${this._photographer.info.price}</p>
+    // </article>
+    // </div>`;
 
-    sectionSolo.innerHTML = photographerSolo;
-      return sectionSolo;
-    }
+    // sectionSolo.innerHTML = photographerSolo;
+    // return sectionSolo;
+  }
 }
