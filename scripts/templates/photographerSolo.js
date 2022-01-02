4b0832b14df272cbb3bb1cb4621699f06bfb44ac
zoanlogia <g.flambard@gmail.com>
2022-01-02 14:41:58 +0100

    added videos & made correction of the names of pic

--- scripts/templates/photographerSolo.js
+++ scripts/templates/photographerSolo.js
@@ -30,38 +30,38 @@ export class PhotographerSolo {
     return div
   }
 
-  createPhotographerSolo() {
-    const sectionSolo = document.createElement("div");
-    sectionSolo.classList.add("container-card");
-    const photographerSolo = `
-    <div class="container-card">
-    <article>
-      <a href="photographer.html?id=${this._photographer.info.id}">
-        <img class="photographer_img" src="assets/photographers/${this._photographer.info.portrait}" alt="${this._photographer.info.name}"/>
-          <h2>${this._photographer.info.name}</h2>
-      </a>
-      <p class="photographer_city">${this._photographer.info.city}, ${this._photographer.info.country}</p>
-      <a href="#">
-        <p class="photographer_tagline">${this._photographer.info.tagline}</p>
-      </a>
-      <p class="photographer_price">${this._photographer.info.price}</p>
-    </article>
-    </div>`;
+  // createPhotographerSolo() {
+  //   const sectionSolo = document.createElement("div");
+  //   sectionSolo.classList.add("container-card");
+  //   const photographerSolo = `
+  //   <div class="container-card">
+  //   <article>
+  //     <a href="photographer.html?id=${this._photographer.info.id}">
+  //       <img class="photographer_img" src="assets/photographers/${this._photographer.info.portrait}" alt="${this._photographer.info.name}" width='350' height="300"/>
+  //         <h2>${this._photographer.info.name}</h2>
+  //     </a>
+  //     <p class="photographer_city">${this._photographer.info.city}, ${this._photographer.info.country}</p>
+  //     <a href="#">
+  //       <p class="photographer_tagline">${this._photographer.info.tagline}</p>
+  //     </a>
+  //     <p class="photographer_price">${this._photographer.info.price}</p>
+  //   </article>
+  //   </div>`;
 
-    sectionSolo.innerHTML = photographerSolo;
-    return sectionSolo;
-  }
+  //   sectionSolo.innerHTML = photographerSolo;
+  //   return sectionSolo;
+  // }
 
   createGallery() {
     const div = document.createElement('div')
 
     const firstname = this._photographer.info.name.split(' ')
-    console.log(firstname);
 
-    let images = ''
+
+    let medias = ''
     this._photographer.medias.forEach(data => {
       if (data.image) {
-        images += `
+        medias += `
         <a href="../assets/${firstname[0]}/${data.image}">
           <img src="../assets/${firstname[0]}/${data.image}" alt="image">
           <div class="gallery__img__desc">
@@ -73,16 +73,17 @@ export class PhotographerSolo {
         </a>`
       }
       else {
-        // videos += `
-        // <a href="../assets/${firstname[0]}/${data.video}">
-        //   <img src="../assets/${firstname[0]}/${data.video}" alt="image">
-        //   <div class="gallery__img__desc">
-        //     <p>${data.title}</p>
-        //     <span>${data.likes}</span>
-        //     <i class="fa-regular fa-heart"></i>
-        //     <i class="fa-solid fa-heart"></i>
-        //   </div>
-        // </a>`;
+        medias += `
+        <a href="../assets/${firstname[0]}/${data.video}">
+          <video controls height="300" width="350">
+          <source src="../assets/${firstname[0]}/${data.video}" type="video/mp4">
+          </video>
+          <div class="gallery__img__desc">
+            <span>${data.likes}</span>
+            <i class="fa-regular fa-heart"></i>
+            <i class="fa-solid fa-heart"></i>
+          </div>
+        </a>`;
       }
     });
 
@@ -103,7 +104,7 @@ export class PhotographerSolo {
             </ul>
           </div>
           <div class="gallery__img">
-          ${images}
+          ${medias}
           </div>`;
     return div
   }
