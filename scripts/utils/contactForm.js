12effff57c73cdf7fb06a63d7b000f8b51e29ebf
zoanlogia <g.flambard@gmail.com>
2022-03-13 13:42:01 +0000

    Revert "save before to make black magic and destroy everything"
    This reverts commit 57c16ba76680b80c3fdb0e1a4bd589b4ab98ead6.

---
+++ scripts/utils/contactForm.js
@@ -0,0 +1,14 @@
+
+// function displayModal() {
+//   btnModal.addEventListener("click", () => {
+//     modal.style.display = "block";
+//   });
+// }
+// displayModal()
+
+// function closeModal() {
+//   Close.addEventListener("click", () => {
+//     modal.style.display = "none";
+//   });
+// }
+// closeModal()
